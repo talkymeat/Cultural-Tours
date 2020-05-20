0d99b33e9c62a76bf34c82cf0e47a8c88f02f4f0
s1903080 <j.xiao-17@sms.ed.ac.uk>
2020-05-21 00:52:35 +0100

    feat: add configuration

--- local/src/config.js
+++ local/src/config.js
@@ -1,3 +1,6 @@
-export const CLIENT_URL = "http://localhost:3001/user/0";
-export const VALIDATOR_URL = "http://localhost:3001/user/1";
-export const SVC_USER_URL = "http://localhost:3001/user/3";
+export const CLIENT_URL =
+  "http://blockchainlab.inf.ed.ac.uk/id-management/#/user/0";
+export const VALIDATOR_URL =
+  "http://blockchainlab.inf.ed.ac.uk/id-management/#/user/1";
+export const SVC_USER_URL =
+  "http://blockchainlab.inf.ed.ac.uk/id-management/#/user/3";
