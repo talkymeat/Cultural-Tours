d926ffc9bd58c665a32a783241ac737bcaf7f062
TalkyMeat <dcochra2@inf.ed.ac.uk>
2020-06-16 13:58:32 +0100

    More fields added to models

--- Cultural-Tours/backend/tours/models.py
+++ Cultural-Tours/backend/tours/models.py
@@ -4,16 +4,22 @@ from django.db import models
 class Site(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
-    lat = models.DecimalField()
-    lon = models.DecimalField()
+    category = models.CharField(max_length=200)
+    subcategory = models.CharField(max_length=200)
+    organisation = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    # event ???
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
     description = models.TextField(null=True)
-    # OTHERS: ADD MORE
+    # Ask K & S about bool/int fields in spreadsheet cols L-AV
 
 class Waypoint(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
-    lat = models.DecimalField()
-    lon = models.DecimalField()
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
 
 class Route(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
