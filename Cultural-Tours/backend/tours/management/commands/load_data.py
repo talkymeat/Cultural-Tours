caf8e0441c769ffdc2162c5ad0c6fdb537da07bd
TalkyMeat <dcochra2@inf.ed.ac.uk>
2020-06-17 05:56:54 +0100

    Method to load sites data from cultural_sites.csv - contd

--- Cultural-Tours/backend/tours/management/commands/load_data.py
+++ Cultural-Tours/backend/tours/management/commands/load_data.py
@@ -3,60 +3,45 @@ from datetime import datetime
 
 from django.core.management import BaseCommand
 
-from adoptions.models import Pet, Vaccine
+from tours.models import Site, Waypoint, Route
 from pytz import UTC
 
 
-DATETIME_FORMAT = '%m/%d/%Y %H:%M'
-
-VACCINES_NAMES = [
-    'Canine Parvo',
-    'Canine Distemper',
-    'Canine Rabies',
-    'Canine Leptospira',
-    'Feline Herpes Virus 1',
-    'Feline Rabies',
-    'Feline Leukemia'
-]
 
 ALREADY_LOADED_ERROR_MESSAGE = """
-If you need to reload the pet data from the CSV file,
+If you need to reload the {} data from the CSV file,
 first delete the db.sqlite3 file to destroy the database.
 Then, run `python manage.py migrate` for a new empty
 database with tables"""
 
 
-class Command(BaseCommand):
+class LoadSites(BaseCommand):
     # Show this when the user types help
-    help = "Loads data from pet_data.csv into our Pet model"
+    help = "Loads data from cultural_sites.csv into our Sites model"
 
     def handle(self, *args, **options):
-        if Vaccine.objects.exists() or Pet.objects.exists():
-            print('Pet data already loaded...exiting.')
-            print(ALREADY_LOADED_ERROR_MESSAGE)
+        if not Site.objects.exists():
+            # print('Site data already loaded...exiting.')
+            # print(ALREADY_LOADED_ERROR_MESSAGE.format('cultural site'))
+            # return
+            print("Loading cultural sites database")
+            for row in DictReader(open('./cultural_sites.csv')):
+                site = Site()
+                site.name = row['Name']
+                site.category = row['Category']
+                site.interest = row['Area']
+                site.subcategory = row['Sub category']
+                site.organisation = row['Organisation']
+                site.address = row['Address']
+                site.website = row['Website']
+                site.lat = row['y']
+                site.lon = row['x']
+                site.description = row['Summary']
+                pet.save()
+        else:
+            # TODO: replace with code that checks for new entries and adds them (?? maybe?)
+            print('Site data already loaded...exiting.')
+            print(ALREADY_LOADED_ERROR_MESSAGE.format('cultural site'))
             return
-        print("Creating vaccine data")
-        for vaccine_name in VACCINES_NAMES:
-            vac = Vaccine(name=vaccine_name)
-            vac.save()
-        print("Loading pet data for pets available for adoption")
-        for row in DictReader(open('./pet_data.csv')):
-            pet = Pet()
-            pet.name = row['Pet']
-            pet.submitter = row['Submitter']
-            pet.species = row['Species']
-            pet.breed = row['Breed']
-            pet.description = row['Pet Description']
-            pet.sex = row['Sex']
-            pet.age = row['Age']
-            raw_submission_date = row['submission date']
-            submission_date = UTC.localize(
-                datetime.strptime(raw_submission_date, DATETIME_FORMAT))
-            pet.submission_date = submission_date
-            pet.save()
-            raw_vaccination_names = row['vaccinations']
-            vaccination_names = [name for name in raw_vaccination_names.split('| ') if name]
-            for vac_name in vaccination_names:
-                vac = Vaccine.objects.get(name=vac_name)
-                pet.vaccinations.add(vac)
-            pet.save()
+
+# TODO: class LoadRoutes(BaseCommand):
