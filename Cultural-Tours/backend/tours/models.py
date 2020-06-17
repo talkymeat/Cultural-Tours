dd2c68be4db761588ac14c0bd594899a3a9e5bc7
TalkyMeat <dcochra2@inf.ed.ac.uk>
2020-06-17 05:55:33 +0100

    Method to load sites data from cultural_sites.csv

--- Cultural-Tours/backend/tours/models.py
+++ Cultural-Tours/backend/tours/models.py
@@ -3,24 +3,32 @@ from django.db import models
 
 class Site(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
-    subcategory = models.CharField(max_length=200)
-    organisation = models.CharField(max_length=200)
-    address = models.CharField(max_length=200)
-    website = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    interest = models.CharField(max_length=50)
+    subcategory = models.CharField(max_length=50)
+    organisation = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    website = models.CharField(max_length=100)
     # event ???
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
     description = models.TextField(null=True)
-    # Ask K & S about bool/int fields in spreadsheet cols L-AV
+    # Outdoors/indoors bool field?
+    # isOpen bool
+    # opening hours ...
+    # level access
+    # local history
+    # N edinburgh gas tower
+    # viewpoints & vistas & architectural treasures
+
 
 class Waypoint(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     lat = models.DecimalField(max_digits=9, decimal_places=6)
     lon = models.DecimalField(max_digits=9, decimal_places=6)
 
 class Route(models.Model):
-    name = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=100, primary_key=True)
     waypoints = models.ManyToManyField('Waypoint')
