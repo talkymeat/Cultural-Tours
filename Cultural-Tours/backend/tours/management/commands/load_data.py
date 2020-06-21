5f6bd962762e2c990a44b292bf2b7b2f0f0dd5df
TalkyMeat <dcochra2@inf.ed.ac.uk>
2020-06-21 14:27:05 +0100

    Method to load sites data, bug fixes

--- Cultural-Tours/backend/tours/management/commands/load_data.py
+++ Cultural-Tours/backend/tours/management/commands/load_data.py
@@ -15,17 +15,15 @@ Then, run `python manage.py migrate` for a new empty
 database with tables"""
 
 
-class LoadSites(BaseCommand):
+class Command(BaseCommand):
     # Show this when the user types help
     help = "Loads data from cultural_sites.csv into our Sites model"
 
     def handle(self, *args, **options):
+
         if not Site.objects.exists():
-            # print('Site data already loaded...exiting.')
-            # print(ALREADY_LOADED_ERROR_MESSAGE.format('cultural site'))
-            # return
             print("Loading cultural sites database")
-            for row in DictReader(open('./cultural_sites.csv')):
+            for row in DictReader(open('./cultural_sites.csv', encoding="utf-8-sig")):
                 site = Site()
                 site.name = row['Name']
                 site.category = row['Category']
@@ -37,7 +35,7 @@ class LoadSites(BaseCommand):
                 site.lat = row['y']
                 site.lon = row['x']
                 site.description = row['Summary']
-                pet.save()
+                site.save()
         else:
             # TODO: replace with code that checks for new entries and adds them (?? maybe?)
             print('Site data already loaded...exiting.')
